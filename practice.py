3d0c006e2f338ef64994ad96b6060bb1fed6ada0
aman <amansingh18112000@gmail.com>
2020-11-29 23:21:57 +0530

    third commit

--- practice.py
+++ practice.py
@@ -1,10 +1,12 @@
 class array:
     def even(self, ):
         self.num = num
-        for number in range(10):
-            print("all number", number)
+        for number in range(1, maximum+1):
+            if(number % 2 == 0):
+                print("all number", number)
 
 
 num = range(20)
 N = array()
+maximum = int(input("enter the maximum number"))
 print(N.even())
